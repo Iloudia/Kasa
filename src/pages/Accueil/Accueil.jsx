3f1cea3891a8ead36527b23b07ba2865ab14f062
Iloudia Vasseur <iloudia.vasseur18@gmail.com>
2025-06-12 14:14:10 +0200

    Page d'accueil + cards

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -1,11 +1,27 @@
-
 import { Link } from 'react-router-dom';
 import './Accueil.css';
+import banner from '../../assets/apropos-banner.jpg';
+import logements from '../../assets/json/maison.json';
 
-function Accueil({}) {
+function Accueil() {
   return (
-    <div>
-       
+    <div className="home">
+      <section className="banner" style={{ backgroundImage: `url(${banner})` }}>
+        <h1><b>Chez vous, partout et ailleurs</b></h1>
+      </section>
+
+      <section className="card-container">
+        {logements.map(({ id, title, cover }) => (
+          <Link
+            key={id}
+            to={`/logement/${id}`}
+            className="location-card"
+            style={{ backgroundImage: `url(${cover})` }}
+          >
+            <span className="location-title">{title}</span>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 }
